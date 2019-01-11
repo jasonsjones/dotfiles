3d57122b40c7d8d295bfeb2cf3971c50b24c66f4
Jason Jones <jsjones96@gmail.com>
2019-01-11 09:17:35 -0800

    Add install script

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+echo $PWD
+
+if [[ $OSTYPE == darwin* ]]; then
+    if [ ! -d $HOME/.vim ]; then
+        echo "Cloning vim config files from github..."
+        git clone git@github.com:jasonsjones/vim-config.git ~/.vim
+    else
+        echo "$HOME/.vim directory already exists, not cloning any configs from github"
+    fi
+    echo "Copying each dotfile to its respective location for MacOS"
+    echo "ln -s $PWD/vimrc ~/.vimrc"
+    ln -s $PWD/vimrc ~/.vimrc
+    # add other macos specific symlinks here, e.g:
+    #ln -s $PWD/zshrc ~/.zshrc
+    #ln -s $PWD/zsh_aliases ~/.zsh_aliases
+    #ln -s $PWD/profile ~/.profile
+    #ln -s $PWD/bash_profile ~/.bash_profile
+    #ln -s $PWD/bashrc ~/.bashrc
+    #ln -s $PWD/gitconfig ~/.gitconfig
+    #ln -s $PWD/hyper.js ~/.hyper.js
+    #ln -s $PWD/tmux.conf ~/.tmux.conf
+    #ln -s $PWD/synery-client.conf ~/.synergy.conf
+elif [[ $OSTYPE == linux* ]]; then
+    echo "Copying each dotfile to its respective location for Linux"
+else
+    echo "This operating system is not supported"
+    exit 1
+fi
