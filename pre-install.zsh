39007d671e5ffd4f8f6c8fd394d4dc022533a46b
Jason Jones <jsjones96@gmail.com>
2022-02-04 15:40:42 -0800

    feat: add spaceship prompt to pre-install script

--- pre-install.zsh
+++ pre-install.zsh
@@ -1,5 +1,8 @@
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/gpakosz/.tmux.git $HOME/.tmux
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH/custom/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH/custom/plugins/zsh-syntax-highlighting"
+git clone https://github.com/gpakosz/.tmux.git "$HOME/.tmux"
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH/custom/themes/spaceship-prompt" --depth=1
+
 ln -sf ~/.tmux/.tmux.conf $HOME/
+ln -s "$ZSH/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/custom/themes/spaceship.zsh-theme"
 
