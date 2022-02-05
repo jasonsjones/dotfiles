39ee76e46f2382ef66903a717de112df04624e74
Jason Jones <jsjones96@gmail.com>
2022-02-05 10:06:41 -0800

    fix: make pre-install script more robust

--- pre-install.zsh
+++ pre-install.zsh
@@ -1,8 +1,13 @@
-git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH/custom/plugins/zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH/custom/plugins/zsh-syntax-highlighting"
-git clone https://github.com/gpakosz/.tmux.git "$HOME/.tmux"
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH/custom/themes/spaceship-prompt" --depth=1
+if [[ -d "$HOME/.oh-my-zsh" ]]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH/custom/plugins/zsh-autosuggestions"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH/custom/plugins/zsh-syntax-highlighting"
+    git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH/custom/themes/spaceship-prompt" --depth=1
+    ln -s "$ZSH/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/custom/themes/spaceship.zsh-theme"
+else
+    echo "Looks like OhmyZsh it not yet installed.  Please install and re-run this script"
+fi
 
-ln -sf ~/.tmux/.tmux.conf $HOME/
-ln -s "$ZSH/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/custom/themes/spaceship.zsh-theme"
+
+git clone https://github.com/gpakosz/.tmux.git "$HOME/.tmux"
+ln -sf $HOME/.tmux/.tmux.conf $HOME/
 
