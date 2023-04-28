fef3e879e81d217e9446fe47eca9ee16c879d6e6
Jason Jones <jsjones96@gmail.com>
2023-04-28 14:14:13 -0700

    refactor: [wip] add autocommands

---
+++ config/nvim/lua/config/autocommands.lua
@@ -0,0 +1,39 @@
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Remove trailing whitespace on save (for all files)
+local TrailingWhitespace = augroup("TrailingWhitespace", { clear = true })
+autocmd({ "BufWritePre" }, {
+    group = TrailingWhitespace,
+    pattern = "*",
+    command = [[%s/\s\+$//e]]
+})
+
+-- Highlight on yank
+local HighlightGroup = augroup("YankHighlight", { clear = true })
+autocmd("TextYankPost", {
+    group = HighlightGroup,
+    pattern = "*",
+    callback = function()
+        vim.highlight.on_yank()
+    end
+})
+
+-- Local setting for markdown files
+local MarkdownConfig = augroup("MarkdownConfig", { clear = true })
+autocmd({ "BufEnter", "BufWinEnter" }, {
+    group = MarkdownConfig,
+    pattern = "*.md",
+    callback = function()
+        vim.cmd("silent! loadview")
+    end
+})
+
+autocmd("BufWinLeave", {
+    group = MarkdownConfig,
+    pattern = "*.md",
+    callback = function()
+        vim.cmd("mkview")
+    end
+})
+
