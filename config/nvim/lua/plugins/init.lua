261c800b519db23764a1406871f83ffcedfe2a3c
Jason Jones <jsjones96@gmail.com>
2023-04-28 16:18:33 -0700

    refactor: add treesitter and lsp-zero to complete nvim config

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -53,5 +53,40 @@ return {
             require("config.telescope")
         end
     },
+
+    -- Treesitter for syntax highlighting and much more...
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function ()
+            require("config.treesitter")
+        end
+    },
+
+    -- LSP setup
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        dependencies = {
+            -- LSP Support
+            {"neovim/nvim-lspconfig"},             -- Required
+            {"williamboman/mason.nvim"},           -- Optional
+            {"williamboman/mason-lspconfig.nvim"}, -- Optional
+
+            -- Autocompletion
+            {"hrsh7th/nvim-cmp"},         -- Required
+            {"hrsh7th/cmp-nvim-lsp"},     -- Required
+            {"hrsh7th/cmp-buffer"},       -- Optional
+            {"hrsh7th/cmp-path"},         -- Optional
+            {"saadparwaiz1/cmp_luasnip"}, -- Optional
+            {"hrsh7th/cmp-nvim-lua"},     -- Optional
+
+            -- Snippets
+            {"L3MON4D3/LuaSnip"},             -- Required
+            {"rafamadriz/friendly-snippets"}, -- Optional
+        },
+        config = function ()
+            require("config.lsp")
+        end
+    }
 }
 
