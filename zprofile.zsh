8687bd1be2e048af8b997d9e3ab93c5acbe79170
Jason Jones <jsjones96@gmail.com>
2023-06-28 10:17:15 -0700

    feat: set path for python 2.7

--- zprofile.zsh
+++ zprofile.zsh
@@ -2,3 +2,8 @@ if [[ $(uname -m) = "arm64" && -e /opt/homebrew/bin/brew ]]; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
+
+# Setting PATH for Python 2.7
+# The original version is saved in .zprofile.pysave
+PATH="/Library/Frameworks/Python.framework/Versions/2.7/bin:${PATH}"
+export PATH
