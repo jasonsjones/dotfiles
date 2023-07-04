fecec905114af27816830edcc69034e93d244f2c
Jason Jones <jsjones96@gmail.com>
2023-07-04 11:11:24 -0700

    refactor: move lualine config to own file

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -35,14 +35,7 @@ return {
     {
         "nvim-lualine/lualine.nvim",
         config = function()
-            require("lualine").setup({
-                options = {
-                    icons_enabled = false,
-                    theme = "nord",
-                    component_separators = "|",
-                    section_separators = ""
-                }
-            })
+            require("config.lualine")
         end
     },
 
