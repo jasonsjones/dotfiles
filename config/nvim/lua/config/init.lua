ad08006986785cb22436b4af35cc1b39ce6f80ac
Jason Jones <jsjones96@gmail.com>
2023-07-05 17:22:25 -0700

    feat: add lua globals file

--- config/nvim/lua/config/init.lua
+++ config/nvim/lua/config/init.lua
@@ -2,6 +2,7 @@ require("config.lazy")
 require("config.options")
 require("config.remap")
 require("config.autocommands")
+require("config.globals")
 require("config.utils")
 require("config.ledger")
 
