79442808c20a7c768e4ab92194ce8eb3c9e3af39
Jason Jones <jsjones96@gmail.com>
2023-11-26 20:53:22 -0800

    feat: add conform plugin for formatters

---
+++ config/nvim/lua/config/conform.lua
@@ -0,0 +1,19 @@
+local status_ok, conform = pcall(require, "conform")
+if not status_ok then
+    return
+end
+
+conform.setup({
+    formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        html = { "prettier" },
+        css = { "prettier" },
+        markdown = { "prettier" },
+    },
+    format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
+})
--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -18,17 +18,17 @@ return {
             vim.g.nord_disable_background = true
 
             require("nord").set()
-        end
+        end,
     },
 
     -- Alpha gretting screen at startup
     {
-        'goolord/alpha-nvim',
+        "goolord/alpha-nvim",
         event = "VimEnter",
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-        config = function ()
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
             require("config.alpha")
-        end
+        end,
     },
 
     -- Updated statusline
@@ -36,15 +36,15 @@ return {
         "nvim-lualine/lualine.nvim",
         config = function()
             require("config.lualine")
-        end
+        end,
     },
 
     -- Git
     {
         "lewis6991/gitsigns.nvim",
-        config = function ()
-            require("gitsigns").setup({ })
-        end
+        config = function()
+            require("gitsigns").setup({})
+        end,
     },
 
     -- Fuzzy Finder (files, lsp, etc)
@@ -52,18 +52,18 @@ return {
         "nvim-telescope/telescope.nvim",
         branch = "0.1.x",
         dependencies = { "nvim-lua/plenary.nvim" },
-        config = function ()
+        config = function()
             require("config.telescope")
-        end
+        end,
     },
 
     -- Treesitter for syntax highlighting and much more...
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        config = function ()
+        config = function()
             require("config.treesitter")
-        end
+        end,
     },
 
     -- LSP setup
@@ -71,61 +71,69 @@ return {
         "VonHeikemen/lsp-zero.nvim",
         dependencies = {
             -- LSP Support
-            {"neovim/nvim-lspconfig"},             -- Required
-            {"williamboman/mason.nvim"},           -- Optional
-            {"williamboman/mason-lspconfig.nvim"}, -- Optional
+            { "neovim/nvim-lspconfig" }, -- Required
+            { "williamboman/mason.nvim" }, -- Optional
+            { "williamboman/mason-lspconfig.nvim" }, -- Optional
 
             -- Autocompletion
-            {"hrsh7th/nvim-cmp"},         -- Required
-            {"hrsh7th/cmp-nvim-lsp"},     -- Required
-            {"hrsh7th/cmp-buffer"},       -- Optional
-            {"hrsh7th/cmp-path"},         -- Optional
-            {"saadparwaiz1/cmp_luasnip"}, -- Optional
-            {"hrsh7th/cmp-nvim-lua"},     -- Optional
+            { "hrsh7th/nvim-cmp" }, -- Required
+            { "hrsh7th/cmp-nvim-lsp" }, -- Required
+            { "hrsh7th/cmp-buffer" }, -- Optional
+            { "hrsh7th/cmp-path" }, -- Optional
+            { "saadparwaiz1/cmp_luasnip" }, -- Optional
+            { "hrsh7th/cmp-nvim-lua" }, -- Optional
 
             -- Snippets
-            {"L3MON4D3/LuaSnip"},             -- Required
-            {"rafamadriz/friendly-snippets"}, -- Optional
+            { "L3MON4D3/LuaSnip" }, -- Required
+            { "rafamadriz/friendly-snippets" }, -- Optional
         },
-        config = function ()
+        config = function()
             require("config.lsp")
-        end
+        end,
+    },
+
+    -- Formatting
+    {
+        "stevearc/conform.nvim",
+        opts = {},
+        config = function()
+            require("config.conform")
+        end,
     },
 
     -- Comments
     {
         "numToStr/Comment.nvim",
-        opts = {}
+        opts = {},
     },
 
     -- Autopairs
     {
         "windwp/nvim-autopairs",
         event = "InsertEnter",
-        opts = {}
+        opts = {},
     },
 
     -- Neorg
     {
-    "nvim-neorg/neorg",
-    build = ":Neorg sync-parsers",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("neorg").setup {
-        load = {
-          ["core.defaults"] = {}, -- Loads default behaviour
-          ["core.concealer"] = {}, -- Adds pretty icons to your documents
-          ["core.dirman"] = { -- Manages Neorg workspaces
-            config = {
-              workspaces = {
-                notes = "~/notes",
-              },
-              default_workspace = "notes"
-            },
-          },
-        },
-      }
-    end,
-    }
+        "nvim-neorg/neorg",
+        build = ":Neorg sync-parsers",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("neorg").setup({
+                load = {
+                    ["core.defaults"] = {}, -- Loads default behaviour
+                    ["core.concealer"] = {}, -- Adds pretty icons to your documents
+                    ["core.dirman"] = { -- Manages Neorg workspaces
+                        config = {
+                            workspaces = {
+                                notes = "~/notes",
+                            },
+                            default_workspace = "notes",
+                        },
+                    },
+                },
+            })
+        end,
+    },
 }
-
