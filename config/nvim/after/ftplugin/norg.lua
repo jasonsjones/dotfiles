781b66d1f96dbdba7cfe878dac6e8ecbc81fc514
Jason Jones <jsjones96@gmail.com>
2023-12-31 01:40:12 -0800

    doc: add comment to .norg (after) ftplugin

--- config/nvim/after/ftplugin/norg.lua
+++ config/nvim/after/ftplugin/norg.lua
@@ -1,2 +1,6 @@
-vim.opt.foldlevel = 2
+-- These neovim options are applied after a `.norg` file is loaded
+
+-- conceallevel 2 is required for named links to conceal
+-- to their file/anchor references or external urls
 vim.opt.conceallevel = 2
+vim.opt.foldlevel = 2
