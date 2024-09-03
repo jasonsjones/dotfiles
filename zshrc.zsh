0e690b9b708df511840c6e1839c1f805b0fbc830
Jason Jones <jsjones96@gmail.com>
2024-09-02 20:46:44 -0700

    fix: remove custom /Users/jasonjones/blt/tools/maven/apache-maven-3.5.4 from ~/blt/tools

--- zshrc.zsh
+++ zshrc.zsh
@@ -89,10 +89,9 @@ fi
 #
 # Personal Exports
 export JAVA_HOME=$(/usr/libexec/java_home)
-export M2_HOME=$HOME/blt/tools/maven/apache-maven-3.5.4
 export VOLTA_HOME="$HOME/.volta"
 # export CC="/usr/bin/clang -std=c++17"
-COMMON_PATH=/opt/X11/bin:$HOME/blt:$HOME/bin:$M2_HOME/bin:$JAVA_HOME/bin:$VOLTA_HOME/bin
+COMMON_PATH=/opt/X11/bin:$HOME/blt:$HOME/bin:$JAVA_HOME/bin:$VOLTA_HOME/bin
 
 # Update env vars whether or not we're runing on the mac studio (M1) or MBP
 if [[ $(hostname -s) == *wsm* ]]; then
